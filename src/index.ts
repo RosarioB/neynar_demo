c516997794a821f3e0b1f8ebefeee51a59da40c7
RosarioB <borgesiros@gmail.com>
2025-03-08 15:55:43 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 3000;
+
+app.use((req: Request, res: Response, next) => {
+  console.log(
+    `${req.method} ${req.path} - ${new Date().toISOString()} - IP: ${req.ip}`
+  );
+  next();
+});
+
+app.set("trust proxy", true);
+
+app.use(express.json());
+
+app.use(cors());
+
+app.post("/", async (req: Request, res: Response) => {
+  try {
+    console.log(req.body);
+    res.send("gm!");
+  } catch (e: any) {
+    res.status(500).send(e.message);
+  }
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, this is a GET request!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
